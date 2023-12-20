1ad55c398ed37c22f34f2e4b82b74d773eb46f9c
Umar Khorami <73532236+obviously-shadow@users.noreply.github.com>
2023-12-20 00:39:29 +0000

    first commit

---
+++ src/utils/bin/certificates.ts
@@ -0,0 +1,5 @@
+export const certs = async (args: string[]): Promise<string> => {
+    window.open(`https://01965b72-97fc-4eb8-8d16-6dee14c3e6c2-00-3p0kn903rtz04.picard.replit.dev/`);
+  
+    return 'Opening Certificates...';
+  };
